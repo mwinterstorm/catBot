f9a5daafbf8581e6932fa8c6f515666486177b2f
mwinterstorm <mark.wr@gmail.com>
2023-10-24 09:55:47 +1300

    Add error handling with homeserver / bot details

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,15 @@ import dotenv from 'dotenv';
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 import { MatrixAuth, MatrixClient, SimpleFsStorageProvider, AutojoinRoomsMixin, RustSdkCryptoStorageProvider } from 'matrix-bot-sdk';
 
-const homeserverUrl = process.env.MATRIX_BASE_URL || '';
-async function login(homeserverUrl: string) {
-    const auth = new MatrixAuth(homeserverUrl);
-    const botUser = process.env.BOT_USER || '';
-    const botPass = process.env.BOT_PASS || '';
-    const authclient = await auth.passwordLogin(botUser, botPass);
-    return authclient.accessToken
-}
-const accessToken = process.env.BOT_TOKEN || '';
 const storage = new SimpleFsStorageProvider("catbot.json");
-if (accessToken != '') {
+
+const homeserverUrl = process.env.MATRIX_BASE_URL || 'invalid_homeserver';
+if (homeserverUrl == 'invalid_homeserver') {
+    throw new Error('Meow! Please add your matrix homeserver URL to your .env file')
+}
+
+const accessToken = process.env.BOT_TOKEN || 'invalid_token';
+if (accessToken != 'invalid_token') {
 
     const cryptoProvider = new RustSdkCryptoStorageProvider("./crypt");
     const client = new MatrixClient(homeserverUrl, accessToken, storage, cryptoProvider);
@@ -32,12 +30,12 @@ if (accessToken != '') {
         if (event['sender'] === await client.getUserId()) return;
 
         // Check to ensure that the `!hello` command is being run
-        if (!body?.startsWith("!hello")) return;
+        // if (!body?.startsWith("!hello")) return;
 
         // Now that we've passed all the checks, we can actually act upon the command
         // console.log("here");
 
-        if (Math.random() <= 0.1) {
+        if (Math.random() <= 0.01) {
             await client.replyNotice(roomId, event, "Hi! It's me CatBot!");
         }
     }
@@ -53,10 +51,15 @@ if (accessToken != '') {
         console.log(`meow! catbot is listening at http://localhost:${port}`);
     });
 } else {
-    login(homeserverUrl).then(token => {
-        console.log("CatBot needs a valid access token");
-        console.log("Copy this access token to your bot's .env: ", token);
-        console.log("Also delete folder /crypt before restart");
-
-    })
+    const auth = new MatrixAuth(homeserverUrl);
+    const botUser = process.env.BOT_USER || 'invalid_user';
+    const botPass = process.env.BOT_PASS || 'invalid_pass';
+    if (botUser == 'invalid_user' || botPass == 'invalid_pass') {
+        throw new Error('Meow! Please register a bot user and add username / password to .env file');
+    }
+    const authclient = await auth.passwordLogin(botUser, botPass);
+    console.log("CatBot needs a valid access token");
+    console.log("Copy this access token to your bot's .env: ", authclient.accessToken);
+    console.log("Also delete folder /crypt before restart");
 }
+export { }
