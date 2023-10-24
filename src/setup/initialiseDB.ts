ed46ea551531eb487d87213bcbafc4bfa30de1c1
mwinterstorm <mark.wr@gmail.com>
2023-10-24 20:14:37 +1300

    ensure db reinitialises on startup

--- src/setup/initialiseDB.ts
+++ src/setup/initialiseDB.ts
@@ -2,11 +2,13 @@ import { reaction } from "../modules/catbotReacts";
 import fs from 'fs'
 
 export async function initialiseDB() {
-    const reactions = (fs.existsSync('config/initReactDb.json.local')) ? JSON.parse(fs.readFileSync('config/initReactDb.json.local', { encoding: 'utf-8' })) : JSON.parse(fs.readFileSync('config/initReactDb.json', { encoding: 'utf-8' }))
+    const reactions = (fs.existsSync('config/initReactDb.json.local')) ? JSON.parse(fs.readFileSync('config/initReactDb.json.local', { encoding: 'utf-8' })) : JSON.parse(fs.readFileSync('config/initReactDb.json', { encoding: 'utf-8' }))    
     for (let i = 0; i < reactions.length; i++) {
-        let reactItem = new reaction(reactions[i])
+        // let reactItem = new reaction(reactions[i])
+        let reactItem = reactions[i]
+        let result = await reaction.findOneAndUpdate({ reactType: reactItem.reactType }, reactItem, { upsert: true, new: true, rawResult: true })
         console.log('meow! initialising ' + reactItem.reactType + ' reactions in DB');
-        reaction.findOneAndUpdate({ reactType: reactItem.reactType }, reactItem, { upsert: true })
+        result.value instanceof reaction
     }
     return
 }
