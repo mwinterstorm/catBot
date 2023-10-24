1f57f5fda764216830220a6bcb677edee4163b39
mwinterstorm <mark.wr@gmail.com>
2023-10-24 16:52:16 +1300

    allow update of defaults in db

--- src/setup/initialiseDB.ts
+++ src/setup/initialiseDB.ts
@@ -2,13 +2,11 @@ import { reaction } from "../modules/catbotReacts";
 import fs from 'fs'
 
 export async function initialiseDB() {
-    const reactions = (fs.existsSync('config/initReactDb.json.local')) ? JSON.parse(fs.readFileSync('config/initReactDb.json.local',{encoding:'utf-8'})) : JSON.parse(fs.readFileSync('config/initReactDb.json',{encoding:'utf-8'}))
+    const reactions = (fs.existsSync('config/initReactDb.json.local')) ? JSON.parse(fs.readFileSync('config/initReactDb.json.local', { encoding: 'utf-8' })) : JSON.parse(fs.readFileSync('config/initReactDb.json', { encoding: 'utf-8' }))
     for (let i = 0; i < reactions.length; i++) {
         let reactItem = new reaction(reactions[i])
-        if (!await reaction.exists({ reactType: reactItem.reactType })) {
-            console.log('meow! initialising ' + reactItem.reactType + ' reactions in DB');
-            await reactItem.save()
-        }
+        console.log('meow! initialising ' + reactItem.reactType + ' reactions in DB');
+        reaction.findOneAndUpdate({ reactType: reactItem.reactType }, reactItem, { upsert: true })
     }
     return
 }
