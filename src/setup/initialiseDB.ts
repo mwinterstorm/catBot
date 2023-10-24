159cd643091bb01ff36e5c36180853d29bc5639d
mwinterstorm <mark.wr@gmail.com>
2023-10-24 20:39:16 +1300

    update log

--- src/setup/initialiseDB.ts
+++ src/setup/initialiseDB.ts
@@ -3,12 +3,13 @@ import fs from 'fs'
 
 export async function initialiseDB() {
     const reactions = (fs.existsSync('config/initReactDb.json.local')) ? JSON.parse(fs.readFileSync('config/initReactDb.json.local', { encoding: 'utf-8' })) : JSON.parse(fs.readFileSync('config/initReactDb.json', { encoding: 'utf-8' }))    
+    let list = []
     for (let i = 0; i < reactions.length; i++) {
-        // let reactItem = new reaction(reactions[i])
         let reactItem = reactions[i]
         let result = await reaction.findOneAndUpdate({ reactType: reactItem.reactType }, reactItem, { upsert: true, new: true, rawResult: true })
-        console.log('meow! initialising ' + reactItem.reactType + ' reactions in DB');
+        list.push(' ' + reactItem.reactType)
         result.value instanceof reaction
     }
+    console.log('meow! initialised' + list + ' reactions in DB');
     return
 }
